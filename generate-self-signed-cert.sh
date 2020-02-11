dda9e22de6b15a93da75499520200f938f6427ba
Tometchy <tometchy@gmail.com>
2020-02-11 15:22:46 +0100

    Use password from environment variable

--- generate-self-signed-cert.sh
+++ generate-self-signed-cert.sh
@@ -3,6 +3,6 @@
 mkdir /out
 
 openssl genrsa 2048 > /out/${DOMAIN}_private.pem
-openssl req -x509 -new -key /out/${DOMAIN}_private.pem -out /out/${DOMAIN}_public.pem -passout pass:password -subj "/C=GB/ST=Nottingham/L=Nottinghamshire/O=SoftwareDeveloper.Blog/OU=IT/CN=${DOMAIN}/emailAddress=tometchy@gmail.com"
-openssl pkcs12 -export -in /out/${DOMAIN}_public.pem -inkey /out/${DOMAIN}_private.pem -out /out/${DOMAIN}.pfx -passout pass:password 
-openssl pkcs12 -in /out/${DOMAIN}.pfx -out /out/${DOMAIN}.crt -nokeys -passin pass:password
+openssl req -x509 -new -key /out/${DOMAIN}_private.pem -out /out/${DOMAIN}_public.pem -passout pass:${PASSWORD} -subj "/C=GB/ST=Nottingham/L=Nottinghamshire/O=SoftwareDeveloper.Blog/OU=IT/CN=${DOMAIN}/emailAddress=tometchy@gmail.com"
+openssl pkcs12 -export -in /out/${DOMAIN}_public.pem -inkey /out/${DOMAIN}_private.pem -out /out/${DOMAIN}.pfx -passout pass:${PASSWORD} 
+openssl pkcs12 -in /out/${DOMAIN}.pfx -out /out/${DOMAIN}.crt -nokeys -passin pass:${PASSWORD}
